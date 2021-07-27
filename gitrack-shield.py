98535febf685f2057a42b1a019db551c4d71f876
ayoub-libs <64891688+ayoub-libs@users.noreply.github.com>
2021-07-27 18:45:47 +0700

    revert

--- gitrack-shield.py
+++ gitrack-shield.py
@@ -57,7 +57,7 @@ def main():
             rules = json.loads(ruleFile.read())
 
         for rule in rules:
-            rules[rule] = re.compile(rules[rule], flags=re.MULTILINE|re.IGNORECASE)
+            rules[rule] = re.compile(rules[rule], flags=re.MULTILINE)
 
     except (IOError, ValueError) as e:
         raise("Error reading rules file")
